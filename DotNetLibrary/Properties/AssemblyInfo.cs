bbd0cec67a24600747e42b437ed5fc1921c5b7cd
OrtPy <ortho.python.2023@gmail.com>
2025-07-21 10:21:04 +0900

    Create AssemblyInfo.cs

---
+++ DotNetLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DotNetLibrary")]
+[assembly: AssemblyDescription("64bit VBA 用 ListView ラッパー")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DotNetLibrary")]
+[assembly: AssemblyCopyright("© YourName")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("00000000-0000-0000-0000-000000000000")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
