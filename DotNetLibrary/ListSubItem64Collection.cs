1eec1237bad79c478f54f4f74ef43540e887173a
OrtPy <ortho.python.2023@gmail.com>
2025-07-21 10:20:36 +0900

    Create ListSubItem64Collection.cs

---
+++ DotNetLibrary/ListSubItem64Collection.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace DotNetLibrary
+{
+    public class ListSubItem64Collection : List<ListViewItem.ListViewSubItem>
+    {
+        // サブアイテム操作を拡張
+    }
+}
